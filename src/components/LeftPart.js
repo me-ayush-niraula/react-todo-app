ff1df0d7738aa11056188a1adb1b512bab698080
Ayush Niraula <115549871+me-ayush-niraula@users.noreply.github.com>
2022-12-25 08:34:07 +0545

    Update LeftPart.js

--- src/components/LeftPart.js
+++ src/components/LeftPart.js
@@ -13,7 +13,7 @@ export const LeftPart = (props) => {
         <h1>My-Todo-App</h1>
       </div>
       <p className="describeText">
-        This is simple React Todo created as a side project, just to get my hands into basics of react including components and props along with some hooks such as useState and useEffect. <br />
+        This is simple React Todo created as a side project, just to get my hands into basics of react including components structure and props along with some hooks such as useState and useEffect. <br />
         Created by:{" "}
         <span className="specialText">
           <a href="https://github.com/me-ayush-niraula">Ayush Niraula</a>
